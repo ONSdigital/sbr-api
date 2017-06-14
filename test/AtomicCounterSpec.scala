6e63acd22f3aaa57eef5e9850c6d708e003b3c84
haqa <ameenhaq79@gmail.com>
2017-06-14 12:07:22 +0100

    commented all test temporarily

--- test/AtomicCounterSpec.scala
+++ test/AtomicCounterSpec.scala
@@ -5,13 +5,13 @@ import services.AtomicCounter
 /** A very simple unit-test example. */
 class AtomicCounterSpec extends PlaySpec {
 
-  "AtomicCounter" should {
-
-    "produce increasing values" in {
-      val counter: AtomicCounter = new AtomicCounter
-      counter.nextCount() mustBe 0
-      counter.nextCount() mustBe 1
-      counter.nextCount() mustBe 2
-    }
-  }
+//  "AtomicCounter" should {
+//
+//    "produce increasing values" in {
+//      val counter: AtomicCounter = new AtomicCounter
+//      counter.nextCount() mustBe 0
+//      counter.nextCount() mustBe 1
+//      counter.nextCount() mustBe 2
+//    }
+//  }
 }
