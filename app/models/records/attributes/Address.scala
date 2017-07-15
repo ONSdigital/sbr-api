99b095095e2e89f4cb634fde951f124ba072c6b4
ameenhaq <mohammed.haq@hotmail.co.uk>
2017-07-15 15:25:35 +0100

    Added Address case field with temp override string

--- app/models/records/attributes/Address.scala
+++ app/models/records/attributes/Address.scala
@@ -6,9 +6,12 @@ import io.swagger.annotations.ApiModelProperty
  * Created by Ameen on 15/07/2017.
  */
 case class Address(
-  @ApiModelProperty(value = "", example = "", dataType = "java.lang.String") line_1: Option[String],
-  @ApiModelProperty(value = "", example = "", dataType = "java.lang.String") line_2: Option[String],
-  @ApiModelProperty(value = "", example = "", dataType = "java.lang.String") line_3: Option[String],
-  @ApiModelProperty(value = "", example = "", dataType = "java.lang.String") line_4: Option[String],
-  @ApiModelProperty(value = "", example = "", dataType = "java.lang.String") line_5: Option[String]
-)
+    @ApiModelProperty(value = "Street and optional door number", example = "101 Long Street") addressLine1: String,
+    @ApiModelProperty(value = "Optional field for building or apartment name", example = "Little Winsor") addressLine2: String,
+    @ApiModelProperty(value = "Town of address", example = "Bury") addressLine3: String,
+    @ApiModelProperty(value = "City of address", example = "Manchester") addressLine4: String,
+    @ApiModelProperty(value = "County of address", example = "Gtr Manchester") addressLine5: String
+) {
+  // FIX replace with JsObject <- JsValue
+  override def toString: String = s"""{"address_line_1":"${addressLine1}","address_line_2":"${addressLine2}","address_line_3":"${addressLine3}","address_line_4":"${addressLine4}","address_line_5":"${addressLine5}"}"""
+}
--- app/utils/MatchObj.scala
+++ app/utils/MatchObj.scala
@@ -1,6 +1,6 @@
 package utils
 
-import models.records.attributes.Matches
+import models.records.attributes.{ Address, Matches }
 import utils.Utilities.getElement
 
 /**
@@ -19,11 +19,7 @@ object MatchObj {
     "ubrn" -> m.ubrn,
     "crn" -> m.crn,
     "idbr" -> m.idbr,
-    "address1" -> m.address1,
-    "address2" -> m.address2,
-    "address3" -> m.address3,
-    "address4" -> m.address4,
-    "address5" -> m.address5,
+    "address" -> m.address,
     "postcode" -> m.postcode,
     "legalStatus" -> m.legalStatus,
     "sic" -> m.sic,
@@ -41,7 +37,7 @@ object MatchObj {
 
   def fromMap(values: Array[String]) =
     Matches(values(0), Option(values(1)), Option(values(2)), Option(values(3).toLong), Option(values(4).toLong),
-      Option(values(5)), Option(values(6).toLong), values(7), values(8), values(9), values(10), values(11), values(12),
+      Option(values(5)), Option(values(6).toLong), Address(values(7), values(8), values(9), values(10), values(11)), values(12),
       Option(values(13).toInt), Option(values(14).toInt), Option(values(15).toInt), Option(values(16).toInt),
       Option(values(17).toInt), Option(values(18).toLong), values(19))
 
