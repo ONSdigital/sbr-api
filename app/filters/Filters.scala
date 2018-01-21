723afe16463b767137cb08889f6daa7353a69c30
Chris Cundill <chrisc@diligentsoft.co>
2018-01-21 22:28:27 +0000

    REG-362 Improved config for cors to be available no matter the env simply by using the appropriate environment variable

--- app/filters/Filters.scala
+++ app/filters/Filters.scala
@@ -3,24 +3,25 @@ package filters
 import javax.inject.Inject
 
 import akka.stream.Materializer
+
 import controllers.BuildInfo
 import play.api.http.DefaultHttpFilters
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.api.mvc.{ Filter, RequestHeader, Result }
 import play.filters.gzip.GzipFilter
-
 import scala.concurrent.Future
 
-class XResponseTimeHeader @Inject() (implicit val mat: Materializer) extends Filter {
+import play.Configuration
+
+class XResponseTimeHeader @Inject() (implicit val mat: Materializer, config: Configuration) extends Filter {
   def apply(nextFilter: RequestHeader => Future[Result])(requestHeader: RequestHeader): Future[Result] = {
     val startTime = System.currentTimeMillis
 
     nextFilter(requestHeader).map { result =>
       val endTime = System.currentTimeMillis
       val responseTime = endTime - startTime
-      val env = sys.props.get("environment").getOrElse("default")
 
-      if (env == "local") {
+      if (config.getBoolean("play.filters.cors.allowAll")) {
         result.withHeaders(
           "X-Response-Time" -> responseTime.toString,
           "Server" -> (BuildInfo.name + "/" + BuildInfo.version),
