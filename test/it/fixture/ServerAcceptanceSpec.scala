8de5fa28c313444994e4dded05c7df7598a830b2
Nigel Perkins <nigelhp@yahoo.co.uk>
2018-06-27 15:42:17 +0100

    Reduce ws connection timeout during acceptance tests to speed up tests

--- test/it/fixture/ServerAcceptanceSpec.scala
+++ test/it/fixture/ServerAcceptanceSpec.scala
@@ -1,6 +1,17 @@
 package fixture
 
 import org.scalatestplus.play.guice.GuiceOneServerPerTest
+import play.api.Application
+import play.api.inject.guice.GuiceApplicationBuilder
 import play.api.test.{ DefaultAwaitTimeout, FutureAwaits }
 
-trait ServerAcceptanceSpec extends AcceptanceSpec with GuiceOneServerPerTest with DefaultAwaitTimeout with FutureAwaits
+trait ServerAcceptanceSpec extends AcceptanceSpec with GuiceOneServerPerTest with DefaultAwaitTimeout with FutureAwaits {
+  /*
+   * A common acceptance scenario is failure handling, and a simple approach to triggering failure conditions is
+   * to shut down the wiremock server that represents some collaborating service.
+   * For some currently unknown reason, this can result in TimeoutExceptions rather than ConnectExceptions.  In the
+   * timeout case, the connection waits for the full timeout period before failing.
+   * We therefore artificially configure a shorter timeout here, to help speed up such acceptance tests.
+   */
+  override def fakeApplication(): Application = new GuiceApplicationBuilder().configure(Map("play.ws.timeout.connection" -> "2500")).build()
+}
