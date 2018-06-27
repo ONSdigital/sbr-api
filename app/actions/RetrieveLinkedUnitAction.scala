91c8fe2c1394e146ba875e55a2fb12648c92577c
Nigel Perkins <nigelhp@yahoo.co.uk>
2018-06-27 16:25:40 +0100

    Log detail message when a service returns a failure

--- app/actions/RetrieveLinkedUnitAction.scala
+++ app/actions/RetrieveLinkedUnitAction.scala
@@ -1,6 +1,7 @@
 package actions
 
 import actions.RetrieveLinkedUnitAction.LinkedUnitRequestActionBuilderMaker
+import com.typesafe.scalalogging.LazyLogging
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.api.mvc._
 import services.{ ErrorMessage, LinkedUnitService }
@@ -14,11 +15,12 @@ object RetrieveLinkedUnitAction {
   type LinkedUnitRequestActionBuilderMaker[T] = (Period, T) => ActionBuilder[LinkedUnitRequest]
 }
 
-class RetrieveLinkedUnitAction[T](linkedUnitService: LinkedUnitService[T]) extends LinkedUnitRequestActionBuilderMaker[T] {
+class RetrieveLinkedUnitAction[T](linkedUnitService: LinkedUnitService[T]) extends LinkedUnitRequestActionBuilderMaker[T] with LazyLogging {
   def apply(period: Period, unitRef: T): ActionBuilder[LinkedUnitRequest] =
     new ActionBuilder[LinkedUnitRequest] {
       override def invokeBlock[A](request: Request[A], block: LinkedUnitRequest[A] => Future[Result]): Future[Result] =
         linkedUnitService.retrieve(period, unitRef).flatMap { errorOrOptLinkedUnit =>
+          errorOrOptLinkedUnit.left.foreach(errorMessage => logger.error(errorMessage))
           block(new LinkedUnitRequest[A](errorOrOptLinkedUnit, request))
         }
     }
