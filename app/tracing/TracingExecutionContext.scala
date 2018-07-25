8e9c69b87063ca4da2df00358a52eebd42033643
Nigel Perkins <nigelhp@yahoo.co.uk>
2018-07-25 11:16:56 +0100

    REG-1250: define a TracingExecutionContext

---
+++ app/tracing/TracingExecutionContext.scala
@@ -0,0 +1,50 @@
+package tracing
+
+import java.util
+
+import com.typesafe.scalalogging.LazyLogging
+import org.slf4j.MDC
+
+import scala.concurrent.{ExecutionContext, ExecutionContextExecutor}
+
+/*
+ * We are using the logback Mapped Diagnostic Context (MDC) to automatically associate a request trace context
+ * with any log statements.  The implementation of MDC however uses a ThreadLocal, assuming a 'one thread per
+ * request' model.  This is not suitable for Play's responsive asynchronous architecture.
+ * This execution context is responsible for migrating the context of a calling thread into any spawned task,
+ * so that a context moves across the threads in the pool as tasks are executed.
+ */
+class TracingExecutionContext(delegateContext: ExecutionContext) extends ExecutionContextExecutor with LazyLogging {
+  override def execute(toRun: Runnable): Unit = {
+    val callingThreadContext = MDC.getCopyOfContextMap
+    logger.debug(s"execute called in thread [${Thread.currentThread().getName}] with a callingThreadContext of [$callingThreadContext]")
+    delegateContext.execute(runnableWithContext(callingThreadContext)(toRun))
+  }
+
+  private def runnableWithContext(callerThreadContext: util.Map[String, String])(toRun: Runnable): Runnable =
+    new Runnable {
+      override def run(): Unit = {
+        val originalThreadContext = MDC.getCopyOfContextMap
+        logger.trace(s"Original context of thread [${Thread.currentThread().getName}] was [$originalThreadContext]")
+        try {
+          setThreadContextTo(callerThreadContext)
+          logger.trace("running task ...")
+          toRun.run()
+        } finally {
+          setThreadContextTo(originalThreadContext)
+        }
+      }
+    }
+
+  private def setThreadContextTo(context: util.Map[String, String]): Unit = {
+    logger.debug(s"Setting context of thread [${Thread.currentThread().getName}] to [$context]")
+    if (context == null) {
+      MDC.clear()
+    } else {
+      MDC.setContextMap(context)
+    }
+  }
+
+  override def reportFailure(cause: Throwable): Unit =
+    delegateContext.reportFailure(cause)
+}
---
+++ test/tracing/TracingExecutionContextSpec.scala
@@ -0,0 +1,101 @@
+package tracing
+
+import com.typesafe.scalalogging.LazyLogging
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.time.{Millis, Seconds}
+import org.scalatest.{FreeSpec, Matchers}
+import org.slf4j.MDC
+
+import scala.concurrent.{ExecutionContextExecutor, Future}
+
+/*
+ * This spec:
+ * - uses the test thread to put a value in the mapped diagnostic context
+ * - kicks off a computation that involves evaluating a number of futures.  These futures will be executed by the
+ *   ForkJoinPool - and we assert that the parent context from the test thread is migrated into each thread from
+ *   the pool that is used to execute a computation.
+ *
+ * As a by-product the computation calculates a factorial, but that is a by-product and is not the purpose of the
+ * test.
+ *
+ * Note that there is no guarantee that different threads from the pool will be used to complete the calculation.
+ * We therefore execute the test many times in the hope that this situation will occur - ensuring that a parent
+ * context was migrated into multiple pool threads.  This is just a hope however - the very nature of concurrency
+ * means that this is non-deterministic.
+ *
+ * If the executionContext used by the fixture is swapped out for the default Scala or Play execution contexts,
+ * this test should fail.
+ */
+class TracingExecutionContextSpec extends FreeSpec with Matchers with ScalaFutures with LazyLogging {
+
+  override implicit val patienceConfig: PatienceConfig = PatienceConfig(
+    timeout = scaled(org.scalatest.time.Span(5, Seconds)),
+    interval = scaled(org.scalatest.time.Span(100, Millis))
+  )
+
+  private trait Fixture {
+    implicit val executionContext: ExecutionContextExecutor = new TracingExecutionContext(scala.concurrent.ExecutionContext.Implicits.global)
+
+    val ContextKey = "SomeKey"
+    private val StartChainValue = 1
+    private val EndChainValue = 40320    // factorial of 8 (8!)
+
+    def testContextMigration(): Unit = {
+      val contextValue = newContextValue
+      logger.trace(s"Setting MDC value to [$contextValue] in thread [${Thread.currentThread().getName}]")
+      MDC.put(ContextKey, contextValue)
+
+      try {
+        val startChain = Future.successful(StartChainValue)
+        whenReady(executeChain(startChain, contextValue)) { n =>
+          n shouldBe EndChainValue
+        }
+        ()
+      } finally {
+        logger.trace(s"Clearing MDC in thread [${Thread.currentThread().getName}]")
+        MDC.clear()
+      }
+    }
+
+    private def newContextValue: String =
+      (Math.random() * 100).toInt.toString
+
+    private def executeChain(startChain: Future[Int], expectedContextValue: String): Future[Int] =
+      startChain.map {
+        (assertContextHasValue(expectedContextValue) _).andThen(multiplyBy(2))
+      }.map {
+        (assertContextHasValue(expectedContextValue) _).andThen(multiplyBy(3))
+      }.map {
+        (assertContextHasValue(expectedContextValue) _).andThen(multiplyBy(4))
+      }.map {
+        (assertContextHasValue(expectedContextValue) _).andThen(multiplyBy(5))
+      }.map {
+        (assertContextHasValue(expectedContextValue) _).andThen(multiplyBy(6))
+      }.map {
+        (assertContextHasValue(expectedContextValue) _).andThen(multiplyBy(7))
+      }.map {
+        (assertContextHasValue(expectedContextValue) _).andThen(multiplyBy(8))
+      }
+
+    /*
+     * This should be invoked on a different thread to that which is running the test.
+     * If everything is working correctly, it should have access to the initial context value, as the top-level
+     * context should have been migrated into any task threads.
+     */
+    private def assertContextHasValue(expectedValue: String)(n: Int): Int = {
+      val actualValue = MDC.get(ContextKey)
+      logger.trace(s"In task thread [${Thread.currentThread().getName}] context has actual value [$actualValue] - expected [$expectedValue]")
+      actualValue shouldBe expectedValue
+      n
+    }
+
+    private def multiplyBy(multiplier: Int)(n: Int): Int =
+      multiplier * n
+  }
+
+  "A TracingExecutionContext" - {
+    "migrates the mapped diagnostic context across tasks" in new Fixture {
+      (1 to 200).foreach(_ => testContextMigration())
+    }
+  }
+}
